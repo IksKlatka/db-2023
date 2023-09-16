bb776cc9701e23adbd866d1181741c8d98dc5eee
Iks Klatka <iga.klatka@gmail.com>
2023-09-16 15:16:48 +0200

    dodanie poprawionych FK

--- migrations_praktyki/versions/cfda7936c3f2_add_persons_fk.py
+++ migrations_praktyki/versions/cfda7936c3f2_add_persons_fk.py
@@ -19,114 +19,165 @@ depends_on = None
 
 def upgrade() -> None:
     op.execute("""
-    
-    ALTER TABLE internship_periods
-        ADD CONSTRAINT internship_periods_student_id__persons_person_id
-            FOREIGN KEY (student_id) REFERENCES persons(person_id);
-    ALTER TABLE internships
-        ADD CONSTRAINT internships_student_id__persons_person_id
-            FOREIGN KEY (student_id) REFERENCES persons(person_id);
-            
-    ALTER TABLE period_final_reports
-        ADD CONSTRAINT period_final_reports_student_id__persons_person_id
-            FOREIGN KEY (student_id) REFERENCES persons(person_id);
-    ALTER TABLE period_final_reports
-        ADD CONSTRAINT period_final_reports_period_id__internship_periods_id
-            FOREIGN KEY (period_id) REFERENCES internship_periods(id);
-
-    ALTER TABLE applications
-        ADD CONSTRAINT applications_student_id__persons__person_id
-            FOREIGN KEY (student_id) REFERENCES persons(person_id);
-
-    ALTER TABLE applications
-        ADD CONSTRAINT applications_cvid__curriculum_vitae_id
-            FOREIGN KEY (cv_id) REFERENCES curriculum_vitae(id);
-
-    ALTER TABLE internship_agreements
-        ADD CONSTRAINT internship_agreements_student_id__persons_person_id
-            FOREIGN KEY (student_id) REFERENCES persons(person_id);
-
-    ALTER TABLE ecdsa_keys
-        ADD CONSTRAINT ecdsa_keys_owner_person_id__persons_person_id
-            FOREIGN KEY (owner_person_id) REFERENCES persons(person_id);
-
-    ALTER TABLE curriculum_vitae
-        ADD CONSTRAINT curriculum_vitae_student_id__persons_person_id
-            FOREIGN KEY (student_id) REFERENCES persons(person_id);
-
-    ALTER TABLE days
-        ADD CONSTRAINT days_period_id__internship_periods_id
-            FOREIGN KEY (period_id) REFERENCES internship_periods(id);
-
-    ALTER TABLE audit_log
-        ADD CONSTRAINT audit_log_author_of_log__persons_person_id
-            FOREIGN KEY (author_of_log) REFERENCES persons(person_id);
-    ALTER TABLE audit_log
-        ADD CONSTRAINT audit_log_target_of_log__persons_person_id
-            FOREIGN KEY (target_of_log) REFERENCES persons(person_id);
-
-    ALTER TABLE signature_texts
-        ADD CONSTRAINT signature_texts_person_id__persons_person_id
-            FOREIGN KEY (person_id) REFERENCES persons(person_id);
-
-    ALTER TABLE signatures
-        ADD CONSTRAINT signatures_person_id__persons_person_id
-            FOREIGN KEY (person_id) REFERENCES persons(person_id);
+        ALTER TABLE applications  
+        ADD CONSTRAINT fk_app_person_id
+        FOREIGN KEY (student_id) 
+        REFERENCES persons(person_id);
         
-    ALTER TABLE survey_answer
-        ALTER COLUMN question TYPE UUID USING question::UUID;
+        ALTER TABLE applications  
+        ADD CONSTRAINT fk_cv_id
+        FOREIGN KEY (cv_id) 
+        REFERENCES curriculum_vitae(id);
+        
+        ALTER TABLE audit_log  
+        ADD CONSTRAINT fk_author_person
+        FOREIGN KEY (author_of_log) 
+        REFERENCES persons(person_id);
+        
+        ALTER TABLE audit_log  
+        ADD CONSTRAINT fk_target_person
+        FOREIGN KEY (target_of_log) 
+        REFERENCES persons(person_id);
+        
+        ALTER TABLE curriculum_vitae  
+        ADD CONSTRAINT fk_cv_person
+        FOREIGN KEY (student_id) 
+        REFERENCES persons(person_id);    
+        
+        ALTER TABLE days  
+        ADD CONSTRAINT fk_internship_periods
+        FOREIGN KEY (period_id) 
+        REFERENCES internship_periods(id);
+        
+        ALTER TABLE ecdsa_keys
+        ADD CONSTRAINT fk_owner_person
+        FOREIGN KEY (owner_person_id)
+        REFERENCES persons(person_id);
+        
+        ALTER TABLE internships
+        ADD CONSTRAINT fk_internship_studentid
+        FOREIGN KEY (student_id)
+        REFERENCES persons(person_id);   
+        
+        ALTER TABLE internship_agreements
+        ADD CONSTRAINT fk_ia_personid
+        FOREIGN KEY (student_id)
+        REFERENCES persons(person_id);   
+        
+        ALTER TABLE internship_agreements
+        ADD CONSTRAINT fk_ia_app_id
+        FOREIGN KEY (application_id)
+        REFERENCES applications(id);     
+        
+        -- dodanie kolumny 
+        ALTER TABLE internship_periods 
+        ADD COLUMN agreement_id UUID;
+        
+        ALTER TABLE internship_periods
+        ADD CONSTRAINT fk_ip_agg_id
+        FOREIGN KEY (agreement_id)
+        REFERENCES internship_agreements(id);     
+        
+        ALTER TABLE internship_periods
+        ADD CONSTRAINT fk_ip_student_id
+        FOREIGN KEY (student_id)
+        REFERENCES persons(person_id);    
+        
+        ALTER TABLE period_final_reports
+        ADD CONSTRAINT fk_pfr_student_id
+        FOREIGN KEY (student_id)
+        REFERENCES persons(person_id);
+        
+        ALTER TABLE period_final_reports
+        ADD CONSTRAINT fk_pfr_period_id
+        FOREIGN KEY (period_id)
+        REFERENCES internship_periods(id);           
+        
+        ALTER TABLE persons
+        ADD CONSTRAINT fk_company_id
+        FOREIGN KEY (company_id)
+        REFERENCES companies(id) ON DELETE SET NULL;  
+        
+        ALTER TABLE signatures
+        ADD CONSTRAINT fk_sig_person_id
+        FOREIGN KEY (person_id)
+        REFERENCES persons(person_id);  
+        
+        ALTER TABLE signatures
+        ADD CONSTRAINT fk_sig_key_id
+        FOREIGN KEY (keyid)
+        REFERENCES ecdsa_keys(key_id);
+        
+        ALTER TABLE signature_texts
+        ADD CONSTRAINT fk_sig_texts_person_id
+        FOREIGN KEY (person_id)
+        REFERENCES persons(person_id);
+        
+        ALTER TABLE survey
+        ADD CONSTRAINT fk_survey_ip_id
+        FOREIGN KEY (internship_period_id)
+        REFERENCES internship_periods(id);
 
-    ALTER TABLE survey_answer
-    ADD CONSTRAINT survey_answer_question__survey_question_id
-        FOREIGN KEY (question) REFERENCES survey_question(id);
     """)
 
 
 def downgrade() -> None:
     op.execute("""
-    ALTER TABLE internship_periods
-        DROP CONSTRAINT internship_periods_student_id__persons_person_id CASCADE;
-    ALTER TABLE internships
-        DROP CONSTRAINT internships_student_id__persons_person_id CASCADE;
-
-    ALTER TABLE period_final_reports
-        DROP CONSTRAINT period_final_reports_student_id__persons_person_id CASCADE;
-    ALTER TABLE period_final_reports
-        DROP CONSTRAINT period_final_reports_period_id__internship_periods_id CASCADE;
-
-    ALTER TABLE applications
-        DROP CONSTRAINT applications_student_id__persons__person_id CASCADE;
-
-    ALTER TABLE applications
-        DROP CONSTRAINT applications_cvid__curriculum_vitae_id CASCADE;
-
-    ALTER TABLE internship_agreements
-        DROP CONSTRAINT internship_agreements_student_id__persons_person_id CASCADE;
-
-    ALTER TABLE ecdsa_keys
-        DROP CONSTRAINT ecdsa_keys_owner_person_id__persons_person_id CASCADE;
-
-    ALTER TABLE curriculum_vitae
-        DROP CONSTRAINT curriculum_vitae_student_id__persons_person_id CASCADE;
-
-    ALTER TABLE days
-        DROP CONSTRAINT days_period_id__internship_periods_id CASCADE;
-
-    ALTER TABLE audit_log
-        DROP CONSTRAINT audit_log_author_of_log__persons_person_id CASCADE;
-    ALTER TABLE audit_log
-        DROP CONSTRAINT audit_log_target_of_log__persons_person_id CASCADE;
-
-    ALTER TABLE signature_texts
-        DROP CONSTRAINT signature_texts_person_id__persons_person_id CASCADE;
-
-    ALTER TABLE signatures
-        DROP CONSTRAINT signatures_person_id__persons_person_id CASCADE;
+        ALTER TABLE applications
+        DROP CONSTRAINT IF EXISTS fk_app_person_id;
+        
+        ALTER TABLE applications
+        DROP CONSTRAINT IF EXISTS fk_cv_id;
+        
+        ALTER TABLE audit_log
+        DROP CONSTRAINT IF EXISTS fk_author_person;
+        
+        ALTER TABLE audit_log
+        DROP CONSTRAINT IF EXISTS fk_target_person;
+        
+        ALTER TABLE curriculum_vitae
+        DROP CONSTRAINT IF EXISTS fk_cv_person;
+        
+        ALTER TABLE days
+        DROP CONSTRAINT IF EXISTS fk_internship_periods;
+        
+        ALTER TABLE ecdsa_keys
+        DROP CONSTRAINT IF EXISTS fk_owner_person;
+        
+        ALTER TABLE internships
+        DROP CONSTRAINT IF EXISTS fk_internship_studentid;
+        
+        ALTER TABLE internship_agreements
+        DROP CONSTRAINT IF EXISTS fk_ia_personid;
+        
+        ALTER TABLE internship_agreements
+        DROP CONSTRAINT IF EXISTS fk_ia_app_id;
+        
+        ALTER TABLE internship_periods
+        DROP CONSTRAINT IF EXISTS fk_ip_agg_id;
+        
+        ALTER TABLE internship_periods
+        DROP CONSTRAINT IF EXISTS fk_ip_student_id;
+        
+        ALTER TABLE period_final_reports
+        DROP CONSTRAINT IF EXISTS fk_pfr_student_id;
+        
+        ALTER TABLE period_final_reports
+        DROP CONSTRAINT IF EXISTS fk_pfr_period_id;
+        
+        ALTER TABLE persons
+        DROP CONSTRAINT IF EXISTS fk_company_id;
+        
+        ALTER TABLE signatures
+        DROP CONSTRAINT IF EXISTS fk_sig_person_id;
+        
+        ALTER TABLE signatures
+        DROP CONSTRAINT IF EXISTS fk_sig_key_id;
+        
+        ALTER TABLE signature_texts
+        DROP CONSTRAINT IF EXISTS fk_sig_texts_person_id;
+        
+        ALTER TABLE survey
+        DROP CONSTRAINT IF EXISTS fk_survey_ip_id;
    
-    ALTER TABLE survey_answer
-        DROP CONSTRAINT survey_answer_question__survey_question_id CASCADE;
-
-    ALTER TABLE survey_answer
-        ALTER COLUMN question TYPE TEXT USING question::TEXT;
-    
     """)
